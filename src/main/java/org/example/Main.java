d9efa4d40d08694cf371579e4ff21e0e60316944
isacGriz <isacgrizante18@gmail.com>
2024-07-01 08:19:33 -0300

    feature(GameLogic): added the game logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,22 @@ package org.example;
 import org.example.DAO.ParameterDAO;
 import org.example.DAO.ParameterDAOImpl;
 import org.example.model.Parameter;
+import org.example.service.GameLogicDAO;
+import org.example.service.GameLogicDAOImpl;
 
 public class Main {
     public static void main(String[] args) {
+
+
         ParameterDAO parameterDAO = new ParameterDAOImpl(args);
         parameterDAO.verifyIfAllMandatoryArgumentsWerePassed();
 
         Parameter parameter = new Parameter();
 
         System.out.println(parameter);
+
+        GameLogicDAO gameLogicDAO = new GameLogicDAOImpl();
+        System.out.println();
+        gameLogicDAO.calculateNextGeneration();
     }
 }
